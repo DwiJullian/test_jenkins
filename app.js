be868d6b1db5dccb1f99bb7cb9cc075f1456523c
julyan dapa <julyandapa@gmail.com>
2024-12-14 13:05:12 +0700

    Update Jenkinsfile with integration tests and deployment

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
-const express = require('express')
-const app = express()
+const request = require('supertest');
+const app = require('./app');
 
-app.get('/', (req, res) => {
-    res.send('Hello, My World!')
-})
+describe('Unit Tests', () => {
+    test('GET / - responds with "Hello, World!"', async () => {
+        const res = await request(app).get('/');
+        expect(res.statusCode).toBe(200);
+        expect(res.text).toBe('Hello, World!');
+    });
 
-module.exports = app
+    test('GET /api/data - responds with JSON data', async () => {
+        const res = await request(app).get('/api/data');
+        expect(res.statusCode).toBe(200);
+        expect(res.body).toEqual({ data: 'This is some data' });
+    });
+
+    test('POST /api/data - returns 201 when item is sent', async () => {
+        const res = await request(app)
+            .post('/api/data')
+            .send({ item: 'New Item' });
+        expect(res.statusCode).toBe(201);
+        expect(res.body).toEqual({ message: 'Data added', item: 'New Item' });
+    });
+});
--- app.test.js
+++ app.test.js
@@ -1,10 +1,25 @@
 const request = require('supertest')
 const app = require('./app')
 
-describe('GET', () => {
+describe('GET & POST', () => {
     it('responds with Hello, My World!', async () => {
         const res = await request(app).get('/')
         expect(res.statusCode).toBe(200)
         expect(res.text).toBe('Hello, My World!')
     })
+    test('End-to-end POST and GET /api/data', async () => {
+        // POST data ke /api/data
+        const postRes = await request(app)
+            .post('/api/data')
+            .send({ item: 'Integration Item' });
+        expect(postRes.statusCode).toBe(201);
+        expect(postRes.body).toEqual({
+            message: 'Data added',
+            item: 'Integration Item',
+        });
+
+        const getRes = await request(app).get('/api/data');
+        expect(getRes.statusCode).toBe(200);
+        expect(getRes.body).toEqual({ data: 'This is some data' }); 
+    });
 })
